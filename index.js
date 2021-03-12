01a727596ee2f8cfd19e7441348fdabf3a94a846
Mateusz Gachowski <mateusz.gachowski@enp.pl>
2021-03-12 12:41:21 +0100

    Log generated id

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ try {
   core.setOutput('generated-id', generatedId);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The id: ${generatedId}`);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
