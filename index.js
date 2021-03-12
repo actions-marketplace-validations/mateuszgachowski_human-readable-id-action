5b2e327b99ea3e454887d170e0c2809920072bcf
Mateusz Gachowski <mateusz.gachowski@enp.pl>
2021-03-12 12:44:44 +0100

    Fix step syntax

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const { hri } = require('human-readable-ids');
 
 try {
   const generatedId = hri.random();
-  core.setOutput('generated-id', generatedId);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The id: ${generatedId}`);
-  console.log(`The event payload: ${payload}`);
+  core.setOutput('id', generatedId);
 } catch (error) {
   core.setFailed(error.message);
 }
